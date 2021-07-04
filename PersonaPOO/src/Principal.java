2ef9b61a7ec6c6c7985cbbe376200279e7471821
Astrik Torres <astriktorres@gmail.com>
2021-07-04 09:28:05 -0500

    .

---
+++ PersonaPOO/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona astrik = new Persona();
+		astrik.name = "Astrik Sem";
+		astrik.age = 19;
+		System.out.println(astrik.saludar());
+		System.out.println(astrik.miEdad());
+	}
+
+}
