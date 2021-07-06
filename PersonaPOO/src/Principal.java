5fac52424196ee9291293ade293cc4f70af0138c
Astrik Torres <astriktorres@gmail.com>
2021-07-06 12:30:26 -0500

    Se cerro un scanner

--- PersonaPOO/src/Principal.java
+++ PersonaPOO/src/Principal.java
@@ -2,7 +2,6 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Persona astrik = new Persona();
 		astrik.name = "Astrik Sem";
 		astrik.age = 19;
